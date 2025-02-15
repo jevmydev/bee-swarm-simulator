03786f3462cd00ae420fb22a15a5130c72cc3a74
Jeremy Díaz <jeremy664k@gmail.com>
2025-02-14 22:32:46 -0300

    Fix route of footer

--- src/pages/Wiki/WikiPage.jsx
+++ src/pages/Wiki/WikiPage.jsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header/Header";
 import Wiki from "../../components/Wiki/Wiki";
-import Footer from "../../components/Footer/footer";
+import Footer from "../../components/Footer/Footer";
 
 export function WikiPage() {
     return (
