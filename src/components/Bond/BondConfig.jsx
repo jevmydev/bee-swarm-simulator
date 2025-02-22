5e112195fb62cafbc9afcef206804d80f272efda
Jeremy Díaz <jeremy664k@gmail.com>
2025-02-21 22:37:02 -0300

    Fix responsive of tables

--- src/components/Bond/BondConfig.jsx
+++ src/components/Bond/BondConfig.jsx
@@ -7,17 +7,7 @@ export function BondConfig({ handleTreatsInventory, handleBondFromTreats, handle
 
     return (
         <form className="flex flex-col sm:flex-row gap-4 justify-between w-full" action="GET" onSubmit={handleSubmit}>
-            <label className="flex items-center gap-4">
-                <span>Treats en tu inventario:</span>
-                <input
-                    className="max-w-20 bg-light py-2 px-4 rounded-lg text-dark text-center font-semibold tracking-wider ring-2 ring-light transition-shadow duration-300 outline-none hover:ring-offset-2 hover:ring-offset-honey"
-                    type="number"
-                    value={treatsInventory}
-                    min={0}
-                    onChange={handleTreatsInventory}
-                />
-            </label>
-            <label className="flex items-center gap-4">
+            <label className="flex flex-wrap items-center gap-4">
                 <span className="max-w-[18.4rem]">Tus &quot;Bond from Treats&quot;: (encuéntralo en tus configuraciones)</span>
                 <input
                     className="max-w-20 bg-light py-2 px-4 rounded-lg text-dark text-center font-semibold tracking-wider ring-2 ring-light transition-shadow duration-300 outline-none hover:ring-offset-2 hover:ring-offset-honey"
@@ -27,7 +17,17 @@ export function BondConfig({ handleTreatsInventory, handleBondFromTreats, handle
                     onChange={handleBondFromTreats}
                 />
             </label>
-            <label className="flex items-center gap-4">
+            <label className="flex flex-wrap items-center gap-4">
+                <span>Treats en tu inventario:</span>
+                <input
+                    className="max-w-20 bg-light py-2 px-4 rounded-lg text-dark text-center font-semibold tracking-wider ring-2 ring-light transition-shadow duration-300 outline-none hover:ring-offset-2 hover:ring-offset-honey"
+                    type="number"
+                    value={treatsInventory}
+                    min={0}
+                    onChange={handleTreatsInventory}
+                />
+            </label>
+            <label className="flex flex-wrap items-center gap-4">
                 <span>N° Abejas:</span>
                 <input
                     className="max-w-20 bg-light py-2 px-4 rounded-lg text-dark text-center font-semibold tracking-wider ring-2 ring-light transition-shadow duration-300 outline-none hover:ring-offset-2 hover:ring-offset-honey"
--- src/components/Bond/BondTable.jsx
+++ src/components/Bond/BondTable.jsx
@@ -3,23 +3,29 @@ export function BondTable({ costRequired }) {
         <table className="table-auto text-center h-max w-full">
             <thead>
                 <tr>
-                    <th className="pb-4 tracking-wider">Level</th>
+                    <th className="pb-4 tracking-wider">
+                        <span className="hidden sm:block">Level</span>
+                        <span className="block sm:hidden">Lvl</span>
+                    </th>
                     <th className="pb-4 tracking-wider">
                         <div className="flex gap-2 justify-center items-center">
                             <img className="w-5 h-5 hidden sm:block" src="/assets/images/items/resources/bond.webp" alt="Bond" loading="lazy" decoding="async" />
-                            Bond Requerido
+                            <span className="hidden sm:block">Bond requerido</span>
+                            <span className="block sm:hidden">Bond req.</span>
                         </div>
                     </th>
                     <th className="pb-4 tracking-wider">
                         <div className="flex gap-2 justify-center items-center">
                             <img className="w-5 h-5 hidden sm:block" src="/assets/images/items/resources/treat.webp" alt="Treat" loading="lazy" decoding="async" />
-                            Treats Requerido
+                            <span className="hidden sm:block">Treats requerido</span>
+                            <span className="block sm:hidden">Treats req.</span>
                         </div>
                     </th>
                     <th className="pb-4 tracking-wider flex gap-2 justify-center items-center">
                         <div className="flex gap-2 justify-center items-center">
                             <img className="w-5 h-5 hidden sm:block" src="/assets/images/items/resources/honey.webp" alt="Honey" loading="lazy" decoding="async" />
-                            Honey Requerida
+                            <span className="hidden sm:block">Honey requerido</span>
+                            <span className="block sm:hidden">Honey req.</span>
                         </div>
                     </th>
                 </tr>
